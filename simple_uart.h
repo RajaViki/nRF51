1e2e7ade28da53cac359f85ab5158c8d3be33bd8
schperplata <domen.jurkovic@gmail.com>
2015-12-25 15:51:57 +0100

    Updated & cleaned version of simple uart.

--- simple_uart.h
+++ simple_uart.h
@@ -1,24 +1,19 @@
- /* Copyright (c) 2009 Nordic Semiconductor. All Rights Reserved.
- *
- * The information contained herein is property of Nordic Semiconductor ASA.
- * Terms and conditions of usage are described in detail in NORDIC
- * SEMICONDUCTOR STANDARD SOFTWARE LICENSE AGREEMENT.
- *
- * Licensees are granted free, non-transferable use of the information. NO
- * WARRANTY of ANY KIND is provided. This heading must NOT be removed from
- * the file.
- *
- *	Updated/customised by Domen Jurkovic, 30.11.2015 
+ /*
+ * Arduino like functions for serial printing of numbers and strings
+ * Created by: Domen Jurkovic, 25.12.2015
+ * 
  */
 
 #ifndef SIMPLE_UART_H
 #define SIMPLE_UART_H
 
-/*lint ++flb "Enter library region" */
-
 #include <stdbool.h>
 #include <stdint.h>
 #include <string.h>	
+
+#include "nrf.h"
+#include "app_uart.h"
+#include "app_error.h"
 
 //uint8_t base:
 #define	DEC	10
@@ -26,57 +21,20 @@
 #define HEX 16
 #define OCT 8
 
-/** @file
-* @brief Simple UART driver
-*
-*
-* @defgroup nrf_drivers_simple_uart Simple UART driver
-* @{
-* @ingroup nrf_drivers
-* @brief Simple UART driver
-*/
+/****************************************************************************************/
+/* UART INIT FUNCTIONS - change in .c file accordingly to your HW */
+/****************************************************************************************/
+// initialize UART
+uint32_t uart_init(uint8_t rx_pin, uint8_t tx_pin, uint32_t baud_rate);
+void uart_error_handler(app_uart_evt_t * p_event);
 
-/** @brief Function for reading a character from UART.
-Execution is blocked until UART peripheral detects character has been received.
-\return cr Received character.
-*/
-uint8_t simple_uart_get(void);
-
-/** @brief Function for reading a character from UART with timeout on how long to wait for the byte to be received.
-Execution is blocked until UART peripheral detects character has been received or until the timeout expires, which even occurs first
-\return bool True, if byte is received before timeout, else returns False.
-@param timeout_ms maximum time to wait for the data.
-@param rx_data pointer to the memory where the received data is stored.
-*/
-bool simple_uart_get_with_timeout(int32_t timeout_ms, uint8_t *rx_data);
-
-/** @brief Function for sending a character to UART.
-Execution is blocked until UART peripheral reports character to have been send.
-@param cr Character to send.
-*/
-void simple_uart_put(uint8_t cr);
-
-/** @brief Function for sending a string to UART.
-Execution is blocked until UART peripheral reports all characters to have been send.
-Maximum string length is 254 characters including null character in the end.
-@param str Null terminated string to send.
-*/
-void simple_uart_putstring(const uint8_t *str);
-
-/** @brief Function for configuring UART to use 38400 baud rate.
-@param rts_pin_number Chip pin number to be used for UART RTS
-@param txd_pin_number Chip pin number to be used for UART TXD
-@param cts_pin_number Chip pin number to be used for UART CTS
-@param rxd_pin_number Chip pin number to be used for UART RXD
-@param hwfc Enable hardware flow control
-*/
-void simple_uart_config(uint8_t rts_pin_number, uint8_t txd_pin_number, uint8_t cts_pin_number, uint8_t rxd_pin_number, bool hwfc);
+uint32_t uart_send_byte(uint8_t byte);
+uint8_t uart_receive_byte(void);
 
 
 /****************************************************************************************/
-/* CUSTOM FUNCTIONS */
+/* PRINT/WRITE FUNCTIONS */
 /****************************************************************************************/
-
 //print WITHOUT new line and carriage return
 void printString(char *data);	//send/print string overserial port UART.
 void printNumber(int32_t number, uint8_t base);	//send/print SINGED/UNSIGNED int32_t number over serial port UART.
@@ -94,8 +52,6 @@ void printUnsignedNumber(uint32_t n, uint8_t base);	//send/print UNSIGNED uint32
 
 void printFloat(float number);
 void printFloatLn(float number);
-
-
 
 /**
  *@}
